fb37830a328d7d494f0c067ce23b2ad3537dbd7e
Tungston-labs <support@tungstonlabs.com>
2025-10-25 14:25:21 +0530

    error are cleared

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,17 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
+// SVG support
 config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
-
 config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== "svg");
 config.resolver.sourceExts.push("svg");
+
+// Add '@' alias for project root
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  "@": path.resolve(__dirname)
+};
 
 module.exports = config;
