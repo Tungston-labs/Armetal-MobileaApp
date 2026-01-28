92e974035ed6f7b4be1b7cadd3fdd4cc4a4e15b0
Ajay-tungston <you@example.com>
2026-01-28 16:49:43 +0530

    Changed the android folder for proper working

--- android/app/src/main/java/com/anonymous/Rekory/LocationService.kt
+++ android/app/src/main/java/com/anonymous/Rekory/LocationService.kt
@@ -135,20 +135,22 @@ class LocationService : Service() {
       .setSmallIcon(R.mipmap.ic_launcher)
       .setOngoing(true)
       .setCategory(NotificationCompat.CATEGORY_SERVICE)
-      .setPriority(NotificationCompat.PRIORITY_LOW)
+.setPriority(NotificationCompat.PRIORITY_DEFAULT)
       .build()
   }
 
-  private fun createNotificationChannel() {
+private fun createNotificationChannel() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      val channel = NotificationChannel(
-        "location_channel",
-        "Location Tracking",
-        NotificationManager.IMPORTANCE_LOW
-      )
-      getSystemService(NotificationManager::class.java)
-        .createNotificationChannel(channel)
+        val channel = NotificationChannel(
+            "location_channel",
+            "Location Tracking",
+            NotificationManager.IMPORTANCE_DEFAULT 
+        )
+        getSystemService(NotificationManager::class.java)
+            .createNotificationChannel(channel)
     }
-  }
+}
+
+
 }
 
